c3c15ff2b6e5fcdfcd56493dd9f4f6d5ba4a4eaa
Ming Wen <ming.wen001@gmail.com>
2018-09-30 02:35:45 -0400

    implemented delete and update operation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from operations import get_table, add_item
+from operations import get_table, add_item, delete_item, update_item
 from shoe import Shoe
 
 """
@@ -6,9 +6,15 @@ driver method for the program
 testing
 """
 
+#delete_item(4, 'shoes')
+updates = {'Size': 7, 'Color': 'White'}
 get_table('shoes')
-#shoe = Shoe('Nike air Jordan 4 levi', 'white', 7, 260, 425)
-#add_item(shoe, 'shoes')
-
+update_item(3, updates, 'shoes')
+get_table('shoes')
+'''
+shoe = Shoe('test', 'test', 7, 260, 425)
+add_item(shoe, 'shoes')
+get_table('shoes')
+'''
 
 
--- operations.py
+++ operations.py
@@ -13,6 +13,12 @@ cursor = db.cursor()
 
 
 def get_table(table_name):
+    """
+    Selects the entire table and display it
+    :param table_name: string
+        the name of the table to display
+    :return: None
+    """
     cursor.execute('SELECT * FROM ' + table_name)
     header_row = cursor.description
     result = cursor.fetchall()
@@ -24,9 +30,65 @@ def get_table(table_name):
 
 
 def add_item(shoe, table_name):
+    """
+    add a new item to a table
+    :param shoe: Shoe Object
+        a Shoe Object encapsulating all the required column data to be inputed into the database
+    :param table_name: string
+        name of the table
+    :return: None
+    """
     sql_statement = 'INSERT INTO ' + table_name + ' (Name, Color, Size, Purchased_Price, Sold_Price)' + \
                     'VALUES (%s, %s, %s, %s, %s)'
     values = (shoe.name, shoe.color, shoe.size, shoe.purchased_price, shoe.sold_price)
+
     cursor.execute(sql_statement, values)
     db.commit()
     print(cursor.rowcount, 'record inserted')
+
+
+def delete_item(item_id, table_name):
+    """
+    delete an item from the database corresponding to a unique id
+    :param item_id: int
+        the unique Id in the database corresponding to the item
+    :param table_name: string
+        name of the table
+    :return: None
+    """
+    sql_statement = 'DELETE FROM ' + table_name + ' WHERE Id = ' + str(item_id)
+
+    cursor.execute(sql_statement)
+    db.commit()
+    print(cursor.rowcount, 'record(s) deleted')
+
+
+def update_item(item_id, updates, table_name):
+    """
+    make an update to a single row in database given the item Id
+    :param item_id: int
+        the unique Id in the database corresponding to the item
+    :param updates: dictionary
+        contains key, value pairs of columns, values to be updated, each key is a column name and values is update val
+    :param table_name: string
+        name of the table
+    :return: None
+    """
+    sql_statement = 'UPDATE ' + table_name + ' SET '
+
+    for key, value in updates.items():
+        if type(value) is int:
+            sql_statement += key + ' = ' + str(value) + ', '
+        else:
+
+            sql_statement += key + ' = \'' + str(value) + '\', '
+
+    sql_statement = sql_statement.rstrip(', ')
+    sql_statement += ' WHERE Id = ' + str(item_id)
+
+    cursor.execute(sql_statement)
+    db.commit()
+    print(cursor.rowcount, 'record(s) updated')
+
+
+def get_table_with_filter(): pass
